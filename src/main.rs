b26b20f9e1722c0ee3709317681398bb40f504f8
shri-acha <happy.irhs@gmail.com>
2025-09-29 07:33:05 +0545

    have to refactor hashmap usage throughout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // core
 mod helpers;
-mod tui;
 use helpers::*;
 use std::collections::{BTreeMap,HashSet};
 use std::{path::{self},fmt::{self,write},io::{self},fs};
@@ -8,7 +7,6 @@ use std::time::Duration;
 use fs_extra::file;
 use notify::{self,Watcher};
 use std::sync::mpsc;
-use tui::*;
 use std::thread::{self};
 use std::sync::Arc;
 
@@ -18,63 +16,36 @@ fn main()->notify::Result<()>{
 
         let (tx, rx) = mpsc::channel::<notify::Result<notify::Event>>();
 
-;
-
         let file_dir_map_buf : BTreeMap<String,Vec<String>> =BTreeMap::from([
-            ("Audio".to_string(),vec!["mp3".to_string()])
+            ("Documents".to_string(),vec!["txt".to_string(),"pdf".to_string()]),
+            ("Video".to_string(),vec!["mov".to_string(),"mp4".to_string()]),
+            ("Audio".to_string(),vec!["wav".to_string(),"mp3".to_string()]),
             ]);
 
+        let (supported_extensions,supported_types) = get_spprtd_extns_and_type(&file_dir_map_buf);
 
         let monitoring_dir: Directory = Directory::new(
-            String::from("/home/shri/.gitbuilds/dirmon/test/test_directory/"),vec![]
+            String::from("/home/shri/.gitbuilds/dirmon/test/test_directory"),vec![]
             );
-        let poll_delay: Duration = Duration::from_secs(1);
 
+        let poll_delay: Duration = Duration::from_secs(1); 
 
-        // monitoring_dir,
-        // dir_buffer,
-        // ext_buffer,
-        // file_dir_map,
-        // focused_field,
-        // exit,
-
-        let mut app = Arc::new(
-            App::new(monitoring_dir,
-                "".to_string(),
-                "".to_string(),
-                file_dir_map_buf,
-                FocusedField::Directory,
-                true,
-                ));
-
-        let (supported_extensions,supported_types): (Vec<String>,Vec<String>) = get_spprtd_extns_and_type(&self.file_dir_map);
-
-    thread::spawn(move||{
-
-        let mut terminal = ratatui::init(); // initializing terminal
-
-        while !app.exit {
-            let _ = app.run(&mut terminal);
-        }
-    ratatui::restore(); 
-    });
- 
+        println!("listening on {:?}",monitoring_dir);
+        println!("supported_types: {:?}\nsupported_extensions: {:?}",supported_extensions, supported_types); 
         
     let mut watcher = notify::PollWatcher::new(tx,
          notify::Config::default()
          .with_poll_interval(poll_delay)
          )?;
-        
-       
-     
-        watcher.watch(&app.monitoring_dir.d_path, notify::RecursiveMode::NonRecursive)?;
-        let mut count: u32= 0;
+         
+        watcher.watch(&monitoring_dir.d_path, notify::RecursiveMode::NonRecursive)?;
+        let mut count: u32=0;
 
-       let files_list: Vec<Box<File>> = get_files(&app.monitoring_dir).unwrap_or(vec![]);
+       let files_list: Vec<Box<File>> = get_files(&monitoring_dir).unwrap_or(vec![]);
         
         // initialization
-        let _ = check_and_write_dir(&app.monitoring_dir,&files_list,&supported_extensions);
-        let _ = move_files(&app.monitoring_dir,&files_list);
+        let _ = check_and_write_dir(&monitoring_dir,&files_list,&supported_extensions);
+        let _ = move_files(&monitoring_dir,&files_list);
 
         for res in rx {
             match res {
@@ -85,8 +56,20 @@ fn main()->notify::Result<()>{
                        continue;
                    }else {
 
-                        let _ = check_and_write_dir(&app.monitoring_dir,&files_list,&supported_extensions);
-                        let _ = move_files(&app.monitoring_dir,&files_list);
+                        if let Ok(_)  = check_and_write_dir(
+                            &monitoring_dir,
+                            &files_list,
+                            &supported_extensions)
+                        {
+                            println!("directory modified!");
+                        }else {
+                            println!("error modifying directory!");
+                        }
+                        if let Ok(_) = move_files(&monitoring_dir,&files_list){
+                            println!("files moved!");
+                        }else {
+                            println!("error moving files!");
+                        }
                         count+=1;
                         println!("[COUNT]: {}\nEvent:{:?}",count,event);
            }
